ae5dab0de8875b536a7d9ba66dcc3b43bf66cbf0
Melisa Anggini <115384615+melisaanggini@users.noreply.github.com>
2023-09-17 11:53:30 +0800

    Add files via upload

---
+++ modul1/PRAK101_2210817220004_MelisaAnggini.java
@@ -0,0 +1,11 @@
+package modul1;
+
+public class PRAK101_2210817220004_MelisaAnggini {
+    public static void main (String[] args){
+
+        System.out.println("Selamat Pagi, Melisa Anggini");
+        System.out.println("Selamat Siang, Melisa Anggini");
+        System.out.println("Selamat Malam, Melisa Anggini");
+
+    }
+}
