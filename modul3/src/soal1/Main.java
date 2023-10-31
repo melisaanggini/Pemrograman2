35cfbeb07d1e14eafb98649e5f8e0229c56cd82f
Melisa Anggini <115384615+melisaanggini@users.noreply.github.com>
2023-10-31 22:10:20 +0800

    Add files via upload

---
+++ modul3/src/soal1/Main.java
@@ -0,0 +1,22 @@
+package soal1;
+
+// Import Scanner
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        // Instansiasi / Membuat Object
+        Dadu dadu = new Dadu();
+
+        // User input banyaknya jumlah dadu (e)
+        int inputUser = input.nextInt();
+
+        dadu.setInput(inputUser);
+
+        dadu.acakNilai();
+        dadu.cetakDadu();
+    }
+}
